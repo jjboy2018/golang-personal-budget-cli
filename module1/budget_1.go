4a6a0c2ddcfe54e96e1a646a52d17f4a85be7d8e
Jason Qian <jason.qian@aexp.com>
2020-11-06 13:19:06 -0700

    Completed module

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,5 +1,17 @@
 package module1
 
+import (
+	"fmt"
+)
+
 // Budget stores budget information
+Budget struct {
+	Max float32 
+	Items []Item
+}
 
 // Item stores item information
+Item struct{
+	Description string
+	Price float32
+}
